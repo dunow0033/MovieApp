1658a7a44cea61e6400afe93126fc26b1fc053b7
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-09-22 11:29:31 +1000

    Add project files.

---
+++ STUDY.MVC.Movies/Data/MoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using STUDY.MVC.Movies.Models;
+
+namespace STUDY.MVC.Movies.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<STUDY.MVC.Movies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
