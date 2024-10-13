9166166f2ae8baf5b71e1b8dfa42b97a4c802dfe
Brian Donnoe <dunow33@hotmail.com>
2024-10-13 16:59:48 -0400

    Switching around some links

---
+++ STUDY.MVC.Movies/Migrations/20241013163019_new database connection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace STUDY.MVC.Movies.Migrations
+{
+    public partial class newdatabaseconnection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- STUDY.MVC.Movies/Program.cs
+++ STUDY.MVC.Movies/Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 
 app.Run();
